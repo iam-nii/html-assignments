917dfc666502b0f61990ce6f0ead70bd95bd40f3
adjeiboyejnr <69145017+Nii-web@users.noreply.github.com>
2023-03-12 18:34:08 +0300

    Update script.js
    User cannot enter two operators consecutively

--- calculator/script.js
+++ calculator/script.js
@@ -2,9 +2,68 @@ function clr()
 {
     document.getElementById("res").value = ""
 }
+
+function isOperator(op)
+{
+    var Element = false;
+    if(op === "+"||op === "-"||op === "*"||op === "/")
+    {
+        Element = true;
+    }
+    return Element;
+}
+
 function display(val)
 {
-    document.getElementById("res").value+=val
+    const temp = document.getElementById('res').value;
+    console.log("temp ->" + temp);
+
+    const lastChar = temp.charAt(temp.length - 1)
+    console.log("lastChar ->" + lastChar);
+
+    const newchar = val;
+    console.log("newchar ->" + newchar);
+
+    var  str = temp;
+
+    console.log("str[length-1] ->" + str[str.length-1]);
+    console.log("newchar ->" + newchar);
+    if(isOperator(str[str.length-1]) && isOperator(newchar))
+    {
+        var newString = temp.substring(0, temp.length -1 ) + val
+        console.log("newString ->" + newString);
+        document.getElementById('res').value = newString;
+        // temp[temp.length - 1] = val
+        // document.getElementById('res').value = temp
+    }
+    else 
+    document.getElementById("res").value+=val;
+        
+
+    //let count = 0;
+    // let length = document.getElementById("res").value.length;
+
+    // console.log("Val ->" + val);
+
+    // let value = document.getElementById("res").value;
+    // let newval = value = document.getElementById("res").value;
+    
+    // console.log("value->" + value);
+    
+
+    // if(isOperator(val) && isOperator(value))
+    // {
+    //     debugger;
+    //      newval[length-1]= val;
+    //      document.getElementById("res").value = newval;
+    //     //console.log("value->" + value);
+    // }
+    // else{
+    //     document.getElementById("res").value+=val;
+    // }
+    
+    
+
 }
 function myevent(event)
 {
@@ -12,11 +71,25 @@ function myevent(event)
         document.getElementById("res").value +=event.key;
     }
     else{
-        document.getElementById("res").value +=event.key;
+        // document.getElementById("res").value +=event.key;
         alert("Please Enter a number");
     }
 }
 function solve()
 {
     document.getElementById("res").value = math.evaluate(document.getElementById("res").value)
+}
+function check()
+{
+    let count = 0;
+    var str_opt = ["+","-","*","/"];
+    for(let i=0;i<document.getElementById("res").lenght;i++)
+    {
+        if((document.getElementById("res").value[count] == '+' ||document.getElementById("res").value[count] == '-' || document.getElementById("res").value[count] == '*' || document.getElementById("res").value[count] == '/') && (document.getElementById("res").value[count +1] == '+' ||document.getElementById("res").value[count+1] == '-' || document.getElementById("res").value[count+1] == '*' || document.getElementById("res").value[count+1]=='/'))
+        {
+            count ++;
+            if(count > 1)
+            break;
+        }
+    }
 }
